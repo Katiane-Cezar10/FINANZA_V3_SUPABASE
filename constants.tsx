332de62eadb8c1e9f32284561b1f228b28cf837a
Katiane-Cezar10 <104310944+Katiane-Cezar10@users.noreply.github.com>
2026-02-17 22:40:06 -0300

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -1,17 +1,5 @@
 import { Asset, FinancialGoal } from './types';
 
 export const INITIAL_ASSETS: Asset[] = [];
+
 export const INITIAL_GOALS: FinancialGoal[] = [];
-
-// Função global segura para moeda
-export const formatCurrency = (value: any) => {
-  const safe =
-    typeof value === "number" && !isNaN(value)
-      ? value
-      : 0;
-
-  return `R$ ${safe.toLocaleString("pt-BR", {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  })}`;
-};
